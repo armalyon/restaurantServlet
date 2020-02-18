ab6b80c7f07c6c8dffd7fa52fd4aecc6a46b84ce
armalyon <a.r.malyon@gmail.com>
2020-02-18 18:53:16 +0200

    14 - pom.xml

--- src/main/java/ua/restaurant/srvlt/model/service/RegFormValidationService.java
+++ src/main/java/ua/restaurant/srvlt/model/service/RegFormValidationService.java
@@ -1,21 +1,17 @@
 package ua.restaurant.srvlt.model.service;
 
-
-import org.apache.log4j.Logger;
 import ua.restaurant.srvlt.exceptions.ConfirmationDoesNotMatchException;
 import ua.restaurant.srvlt.exceptions.RegexMismatchException;
 import ua.restaurant.srvlt.model.dto.AccountDTO;
 import ua.restaurant.srvlt.model.service.utility.ValidationUtility;
 
 public class RegFormValidationService {
-    private static final Logger LOGGER = Logger.getLogger(RegFormValidationService.class);
 
     private ValidationUtility validationUtility = new ValidationUtility();
 
 
     public boolean isRegFormValid(AccountDTO accountDTO)
             throws ConfirmationDoesNotMatchException, RegexMismatchException {
-        LOGGER.debug(accountDTO);
         return validationUtility
                 .isPasswordCanBeUsed(accountDTO.getPassword(), accountDTO.getPasswordConfirmation())
                 && validationUtility
